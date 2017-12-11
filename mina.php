e115e7f4536ec0f917bcedd051fce58f77b8bd50
Lukazenko <veikko.kiis@khk.ee>
2017-12-11 09:54:21 +0200

    Eesnime defineerimine ja väljastamine

---
+++ mina.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: asd
+ * Date: 12/11/2017
+ * Time: 8:51 AM
+ */
+
+$asd = "Veikko";
+echo "<b>"."Minu nimi on " . $asd."</b><br />";
+
+// või
+
+print "<b>"."Minu nimi on " . $asd."</b><br />";
+
+ // või
+
+print("Minu nimi on $asd<br />");
+?>
+
+
+
+
+
