8a321697fb9776c3fd12b83f9b85c0947680b686
Lukazenko <veikko.kiis@khk.ee>
2017-12-15 09:23:01 +0200

    värvide lisamine

--- funktsioonid.php
+++ funktsioonid.php
@@ -8,22 +8,36 @@
 
 
 
-function lootabel($ridadeArv, $veerudeArv){
+function lootabel($ridadeArv, $veergudeArv){
     echo '<table border="1">';
         for($reanumber = 1; $reanumber <= $ridadeArv; $reanumber++){
             echo '<tr>';
-            for($veeruNumber = 1; $veeruNumber <= $veerudeArv; $veeruNumber++){
-                echo '<td>';
+            for($veeruNumber = 1; $veeruNumber <= $veergudeArv; $veeruNumber++){
+                echo '<td style="background-color: '.genereeriVarv().';">';
                 echo rand(10,99);
                 echo '</td>';
             }
             echo '</tr>';
         }
-    echo '</table';
+    echo '</table>';
 }
 
 lootabel(5, 7);
 
 
 
-?>
+function genereeriVarv(){
+        $varv = '#';
+        for($kord = 1; $kord <= 6; $kord++){
+            $juhuarv = rand(0,15);
+            $juhuhex = dechex($juhuarv);
+            $varv = $varv.$juhuhex;
+        }
+    return $varv;
+}
+
+echo genereeriVarv();
+
+
+
+
