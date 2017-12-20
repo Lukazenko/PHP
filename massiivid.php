865b541ee0f6e57277797b9f846dddad0588cc24
Lukazenko <veikko.kiis@khk.ee>
2017-12-20 12:09:24 +0200

    massiiv

---
+++ massiivid.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: asd
+ * Date: 12/20/2017
+ * Time: 12:02 PM
+ */
+$arvud = array(1,5,2,4,3);
+var_dump($arvud);
+echo '<pre>';
+print_r($arvud);
+echo '</pre>';
+
+
