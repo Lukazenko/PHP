3e4ee9dd65593b68fe15e09a9351196cf47821d9
Lukazenko <veikko.kiis@khk.ee>
2018-01-08 08:58:17 +0200

    foreach väljastus

--- Sidestatud massiiv.php
+++ Sidestatud massiiv.php
@@ -8,3 +8,18 @@
 
 
 $opilane = array('eesnimi' => 'Mart', 'perenimi' => 'Lepp', 'vanus' => 16, 'klass' => 10);
+
+
+echo '<pre>';
+print_r($opilane);
+echo '</pre>';
+
+echo '<hr />';
+
+
+//väljastab võtmed ja väärtused foreach tsükliga
+
+
+foreach($opilane as $voti => $vaartus){
+    echo $voti.' - '.$vaartus.'<br />';
+}
