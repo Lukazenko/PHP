5c4fd1d97d9094417799d304090712d8acd48853
Lukazenko <veikko.kiis@khk.ee>
2018-01-08 08:51:25 +0200

    sidestatud massiiv

---
+++ Sidestatud massiiv.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: asd
+ * Date: 1/8/2018
+ * Time: 8:44 AM
+ */
+
+
+$opilane = array('eesnimi' => 'Mart', 'perenimi' => 'Lepp', 'vanus' => 16, 'klass' => 10);
