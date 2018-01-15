96273e6cf9dee6698fee31190a2c2803bc9d1aab
Lukazenko <veikko.kiis@khk.ee>
2018-01-15 12:27:49 +0200

    Tehtud klass ja defineeritud klassi muutujad

---
+++ klassid/tabel.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: asd
+ * Date: 1/15/2018
+ * Time: 12:24 PM
+ */
+
+class tabel
+{
+    // klassi muutujad
+
+    var $tabeliSisu = array();
+    var $pealkirjad = array();
+    var $veergudeArv;
+
+    
+
+}
